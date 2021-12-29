7714537500e96b2e53463f0d6b06ae36f8cffeb6
ajaylee <ajaidonglee@gmail.com>
2021-12-29 20:48:31 +0530

    helicopter challenge

---
+++ src/HelicopterChallange.java
@@ -0,0 +1,48 @@
+package main.java;
+
+public class HelicopterChallange {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("                                :LOL:ROFL:ROFL:ROFL");
+            System.out.println("                                  A");// 10 tab
+            System.out.println("                   ______________________________"); // 5 tab
+            System.out.println("  L                \\                           []\\");
+            System.out.println(" LOL==================                            \\");
+            System.out.println("                     \\                             \\");
+            System.out.println("                      \\ ____________________________]");
+            System.out.println("                           I                       I");
+            System.out.println("                        - - - - - - - - - - - - - - - - - -/");
+            Thread.sleep(100);
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("                  ROFL:ROFL:ROFL:LOL:");
+            System.out.println("                                  A");// 10 tab
+            System.out.println("                   ______________________________"); // 5 tab
+            System.out.println("                   \\                           []\\");
+            System.out.println(" LOL==================                            \\");
+            System.out.println("  L                  \\                             \\");
+            System.out.println("                      \\ ____________________________]");
+            System.out.println("                           I                       I");
+            System.out.println("                        - - - - - - - - - - - - - - - - - -/");
+            Thread.sleep(100);
+
+        }
+    }
+
+}
---
+++ src/LeetCode/RemoveDuplicateFromArray.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateFromArray {
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 3, 3, 4, 5 };
+        System.out.println(removeDuplicates(nums));
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (i == nums.length)
+                return count;
+            if (nums[i - 1] == nums[i]) {
+                nums[i] = nums[i + 1];
+                count++;
+            }
+        }
+        return count;
+    }
+}
