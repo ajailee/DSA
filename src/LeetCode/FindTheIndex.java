0369bd23a3d45e834bfb84d7563e025679885a53
ajaylee <ajaidonglee@gmail.com>
2022-01-02 13:57:10 +0530

    new year resolution

---
+++ src/LeetCode/FindTheIndex.java
@@ -0,0 +1,43 @@
+package LeetCode;
+
+public class FindTheIndex {
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 2;
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int mid = nums.length / 2;
+        if (nums[mid] == target)
+            return mid;
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        if (nums[mid] > target) {
+            for (int i = 0; i <= mid; i++) {
+                if (nums[i] == target) {
+                    return i;
+                } else if (target < nums[i]) {
+                    return i;
+                }
+
+                else if (nums[i] <= target && target <= nums[i + 1]) {
+                    return i + 1;
+                }
+            }
+        } else {
+            for (int i = mid; i < nums.length - 1; i++) {
+                if (nums[i] == target) {
+                    return i;
+                } else if (target > nums[nums.length - 1]) {
+                    return nums.length;
+                } else if (nums[i] <= target && target <= nums[i + 1]) {
+                    return i + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
