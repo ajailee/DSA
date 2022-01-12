8a384c303b3548b00c38bbd34edc47a67700863e
ajaylee <ajaidonglee@gmail.com>
2022-01-12 15:20:54 +0530

    nyr

---
+++ src/LeetCode/LeetCode/MaximumDepthofBinaryTree.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class MaximumDepthofBinaryTree {
+    public static int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = 1 + maxDepth(root.left);
+        int right = 1 + maxDepth(root.right);
+        return Math.max(left, right);
+    }
+
+    public static int maxDepth1(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+    }
+
+}
