3ce50f5f5ff3b757ff3081030cca203f5e54b074
ajaylee <ajaidonglee@gmail.com>
2022-01-26 13:31:38 +0530

    nyr

--- src/LeetCode/LeetCode/SingleNumber.java
+++ src/LeetCode/LeetCode/SingleNumber.java
@@ -2,7 +2,11 @@ package LeetCode;
 
 public class SingleNumber {
     public static void main(String[] args) {
-        int[] a = new int[] { 1, 3, 3 };
-
+        int[] a = new int[] { 1, 3, 1, 3, 5 };
+        int result = 0;
+        for (int i = 0; i < a.length; i++) {
+            result = result ^ a[i];
+        }
+        System.out.println(result);
     }
 }
