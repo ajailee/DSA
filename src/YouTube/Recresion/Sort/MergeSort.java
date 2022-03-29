c9f57f606c3158b829a49f442f573d91788d32f3
ajaylee <ajaidonglee@gmail.com>
2022-03-29 14:30:42 +0530

    test

---
+++ src/YouTube/Recresion/Sort/MergeSort.java
@@ -0,0 +1,58 @@
+package Recresion.Sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MergeSort {
+  public static void main(String[] args) {
+    int arr[] = {4,2,6,10,14,13,1,0};
+        int[] result = split(arr);
+        System.out.println(Arrays.toString(result));
+}
+
+  public  static void inplaceSplit(int[] arr,int start,int end){
+      if(end)
+  }
+
+  public static  int[] split(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left = split(Arrays.copyOfRange(arr,0,mid));
+        int[] right = split(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+  }
+
+  public static int[] merge(int[] left,int[] right){
+        int lp = 0;
+        int rp = 0;
+        int np=0;
+        int[] result = new int[right.length+left.length];
+        while (lp<left.length && rp<right.length){
+            if(left[lp]<right[rp]){
+                result[np] = left[lp];
+                lp++;
+            }else{
+                result[np] = right[rp];
+                rp++;
+            }
+            np++;
+        }
+        while (lp<left.length){
+            result[np] = left[lp];
+            lp++;
+            np++;
+        }
+        while (rp<right.length){
+            result[np]=right[rp];
+            rp++;
+            np++;
+        }
+        return result;
+  }
+}
+
