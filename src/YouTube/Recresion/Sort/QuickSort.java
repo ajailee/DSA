79fb883e271b78f9c54eb1ba437887c84b8f59c0
ajaylee <ajaidonglee@gmail.com>
2022-04-05 08:09:22 +0530

    nyr

--- src/YouTube/Recresion/Sort/QuickSort.java
+++ src/YouTube/Recresion/Sort/QuickSort.java
@@ -1,7 +1,43 @@
 package Recresion.Sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
   public static void main(String[] args) {
-    //
+    int[] arr = {5,6,3,2,1};
+    int start=0;
+    int end=arr.length-1;
+    quickSort(arr,start,end);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  private static void quickSort(int[] arr, int start, int end) {
+      int s=start;
+      int e=end;
+      int mid = start +(end-start) /2;
+      int pivot = arr[mid];
+      if(start>=end){
+          return;
+      }
+      while(s<=e){
+          while (arr[s]<pivot){
+              s++;
+          }
+          while (arr[e]>pivot){
+              e--;
+          }
+          if(s<=e){
+              swap(s,e,arr);
+              s++;
+              e--;
+          }
+      }
+      quickSort(arr,start,e);
+      quickSort(arr,s,end);
+  }
+  static void swap(int first,int second,int[] arr){
+    int temp = arr[first];
+    arr[first]=arr[second];
+    arr[second]=temp;
   }
 }
