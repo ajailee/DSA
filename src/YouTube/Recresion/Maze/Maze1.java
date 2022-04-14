1cf53d479dff31ab36a30eae90e453c8776d92bd
Ajay Lee <ajaidonglee@gmail.com>
2022-04-14 19:47:42 +0530

    nyr

--- src/YouTube/Recresion/Maze/Maze1.java
+++ src/YouTube/Recresion/Maze/Maze1.java
@@ -5,6 +5,8 @@ public class Maze1 {
         int n = 3;
         int target = 9;
         find(n, target, 1, "");
+        System.out.println("proFind");
+        proFind(new Position(2, 2), new Position(0, 0), new Position(2, 2), "");
     }
 
     static void find(int n, int target, int i, String processed) {
@@ -21,6 +23,62 @@ public class Maze1 {
         if (!(i < n * n && i > ((n * n) - n))) {
             find(n, target, i + n, processed + "D");
         }
+    }
+
+    static void proFind(Position maze, Position current, Position target, String processed) {
+        if (current.i > maze.i || current.j > maze.j) {
+            return;
+        }
+        if (target.equals(current)) {
+            System.out.println(processed);
+            return;
+        }
+        if (current.i < maze.i && current.j < maze.j) {
+            proFind(maze, current.diagonally(), target, processed + "D");
+        }
+        if (current.i < maze.i) {
+            proFind(maze, current.down(), target, processed + "V");
+        }
+        if (current.j < maze.j) {
+            proFind(maze, current.right(), target, processed + "H");
+        }
+    }
+
+}
+
+class Position {
+    int i;
+    int j;
+
+    Position(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    Position down() {
+        int newi = this.i + 1;
+        return new Position(newi, this.j);
+    }
+
+    Position right() {
+        int newj = this.j + 1;
+        return new Position(i, newj);
+    }
+
+    Position diagonally() {
+        int newi = this.i + 1;
+        int newj = this.j + 1;
+        return new Position(newi, newj);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Position))
+            return false;
+        Position obj1 = (Position) obj;
+        return (obj1.i == this.i && obj1.j == this.j);
     }
 
 }
---
+++ src/YouTube/Recresion/Maze/Maze2.java
@@ -0,0 +1,72 @@
+package Recresion.Maze;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Maze2 {
+    public static void main(String[] args) {
+
+        Position2 maze = new Position2(2, 2);
+        Position2 target = new Position2(2, 2);
+        Position2 current = new Position2(0, 0);
+        Position2 obstacle = new Position2(1, 1);
+        Position2 obstacle1 = new Position2(0, 1);
+        List<Position2> obstracleList = new ArrayList<>();
+        obstracleList.add(obstacle);
+        obstracleList.add(obstacle1);
+        find(maze, current, target, "", obstracleList);
+
+    }
+
+    static void find(Position2 maze, Position2 current, Position2 target, String processed, List<Position2> obstacle) {
+        if (current.i > maze.i || current.j > maze.j) {
+            return;
+        }
+        if (current.equals(target)) {
+            System.out.println(processed);
+            return;
+        }
+        if (current.i < maze.i && (!obstacle.contains(current.down()))) {
+            Position2 newc = current.down();
+            find(maze, newc, target, processed + "D", obstacle);
+        }
+        if (current.j < maze.j && (!obstacle.contains(current.right()))) {
+            find(maze, current.right(), target, processed + "R", obstacle);
+        }
+        if (current.j < maze.j && current.i < maze.i && (!obstacle.contains(current.diagonally()))) {
+            find(maze, current.diagonally(), target, processed + "d", obstacle);
+        }
+    }
+}
+
+class Position2 {
+    int i;
+    int j;
+
+    Position2(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    Position2 down() {
+        int newi = this.i + 1;
+        return new Position2(newi, this.j);
+    }
+
+    Position2 right() {
+        int newj = this.j + 1;
+        return new Position2(this.i, newj);
+    }
+
+    Position2 diagonally() {
+        int newi = this.i + 1;
+        int newj = this.j + 1;
+        return new Position2(newi, newj);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Position2 p = (Position2) obj;
+        return (p.i == this.i && p.j == this.j);
+    }
+}
